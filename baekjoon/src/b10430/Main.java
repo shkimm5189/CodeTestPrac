2cc29698c4b80ac45fa80450192ced9474cf2da3
shkimm5189 <shkimm5189@gmail.com>
2021-01-06 12:38:16 +0900

    B10430 - 나머지 해결 / 모듈러 연산 이해

---
+++ baekjoon/src/b10430/Main.java
@@ -0,0 +1,40 @@
+package b10430;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+ 문제
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+
+출력
+첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		
+		bw.write((A+B)%C+"\n");
+		bw.write(((A%C) + (B%C))%C+"\n");
+		bw.write((A*B)%C+"\n");
+		bw.write(((A%C) * (B%C))%C+"\n");
+		bw.flush();
+		bw.close();
+	}
+	
+}
