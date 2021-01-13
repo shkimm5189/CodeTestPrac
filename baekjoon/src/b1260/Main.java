71b7ef8304e4d4ba98051f9bbc7b171783a62b98
shkimm5189 <shkimm5189@gmail.com>
2021-01-13 15:02:13 +0900

    B1260 해결 완료

--- baekjoon/src/b1260/Main.java
+++ baekjoon/src/b1260/Main.java
@@ -1,5 +1,64 @@
 package b1260;
 
-public class Main {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class Main {
+	static int board[][];
+	static boolean visited[];
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		board = new int[N+1][N+1]; 
+		int start  = sc.nextInt();
+		for(int i = 1 ; i <= M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			board[a][b] = 1 ;
+			board[b][a] = 1 ;
+		}
+		
+		visited = new boolean[N+1];
+		dfs(start);
+		
+		visited = new boolean[N+1];
+		bfs(start);
+	}
+	static void dfs(int start) {
+		visited[start] = true;
+		System.out.println(start + " " );
+		if(start == board.length) {
+			return ;
+		}
+		
+		for(int i=1; i < board.length ; i++) {
+			if(board[start][i] == 1 && visited[i] == false) {
+				dfs(i);
+			}
+		}
+	}
+	static void bfs(int start) {
+		Queue<Integer> que = new LinkedList<Integer>();
+		que.offer(start);
+		visited[start] = true;
+		System.out.println(start+ " ");
+		
+		while(!que.isEmpty()) {
+			int temp = que.peek();
+			que.poll();
+			for(int i = 1 ; i < board.length; i++) {
+				if(board[temp][i] == 1 && visited[i] == false) {
+					que.offer(i);
+					visited[i] = true;
+					System.out.println(i+ " ");
+				}
+			}
+		}
+		
+	}
 }
