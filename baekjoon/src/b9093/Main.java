176c64fdf216af1490fa3e3a651531f761a35a89
shkimm5189 <shkimm5189@gmail.com>
2021-01-18 14:31:58 +0900

    B9093 단어 뒤집기 해결

--- baekjoon/src/b9093/Main.java
+++ baekjoon/src/b9093/Main.java
@@ -1,5 +1,35 @@
 package b9093;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		String[] str;
+		
+		int repeat = Integer.parseInt(br.readLine());
+		
+		for(int i = 0 ; i < repeat; i++) {
+			str = br.readLine().split(" ");
+			for(int j = 0 ; j < str.length; j++) {
+				for(int z = str[j].length() - 1 ; z >= 0 ; z--) {
+					char temp = str[j].charAt(z);
+					sb.append(temp);
+				}
+				
+			}
+			
+		}
+		bw.write(sb.toString());
+		br.close();
+		bw.flush();
+		bw.close();
+		
+	}
 }
