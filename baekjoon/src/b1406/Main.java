96428d5694940547ddd14fd43ffc274c51d59724
shkimm5189 <shkimm5189@gmail.com>
2021-01-21 14:35:32 +0900

    B1406 에디터 해결  - stack 2개사용으로 해결

--- baekjoon/src/b1406/Main.java
+++ baekjoon/src/b1406/Main.java
@@ -1,10 +1,13 @@
 package b1406;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 /*
@@ -16,14 +19,15 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
 		int repeat;
-		
+
 		Editor edit = new Editor(br.readLine());
 		repeat = Integer.parseInt(br.readLine());
 		while(repeat-- > 0) {
-		
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String chk = st.nextToken();
 			switch(chk) {
@@ -41,20 +45,23 @@ public class Main {
 				break;
 			}
 		}
-		
-		System.out.println(edit.toString());
+		br.close();
+		bw.write(edit.toString());
+		bw.flush();
+		bw.close();
 	}
 }
 class Editor{
-	StringBuilder sb;
-	int cursor;
+	Stack<String> rightSt;
+	Stack<String> leftSt;
 	Editor(String str){
-		sb = new StringBuilder();
-		String[] temp = str.split("");
-		for(String s : temp) {
-			sb.append(s);
+		rightSt = new Stack<>();
+		leftSt = new Stack<>();
+			
+		String[] atr = str.split("");
+		for(String s : atr) {
+			leftSt.push(s);
 		}
-		cursor = sb.length();
 	}
 	/*
 	 * 문자열로 가는게맞는가?
@@ -64,47 +71,44 @@ class Editor{
 	 * L 커서 왼쪽으로 한칸 "문장 맨앞이면 무시"
 	 */
 	public void L() {
-		if(cursor != 0) {
-			cursor--;
+		if(!leftSt.empty()) {
+			rightSt.push(leftSt.pop());
 		}
-		
-		
 	}
 	/*
 	 * D 커서 오른쪽 한칸 "문장 맨뒤면 무시"
 	 */
 	public void D() {
-		if(cursor != sb.length()) {
-			cursor++;
+		if(!rightSt.empty()) {
+			leftSt.push(rightSt.pop());
 		}
-		
+
 	}
 	/*
 	 * B 커서 왼쪽 문자 삭제 "문장 맨앞이면 무시"
 	 */
 	public void B() {
-		
-		if(cursor!=0) {
-			sb.delete(cursor-1, cursor);
-			cursor--;
+		if(!leftSt.empty()) {
+			leftSt.pop();
 		}
-		
 	}
 	/*
 	 * P $ 커서 오른쪽에 $문자 추가
 	 */
 	public void P(String s) {
-		
-		sb.insert(cursor , s);
-		cursor++;
+		leftSt.push(s);
 	}
 	
 	@Override
-		public String toString() {
-		
-			return sb.toString();
-			
+	public String toString() {
+		// TODO Auto-generated method stub
+		StringBuilder sb =new StringBuilder();
+		while(!leftSt.empty()) {
+			rightSt.push(leftSt.pop());
 		}
-	
-	
+		while(!rightSt.empty()) {
+				sb.append(rightSt.pop());
+		}
+		return sb.toString();
+	}
 }
