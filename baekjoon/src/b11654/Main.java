5b4eab6fb9bfc7d7d875d50b4735f59ad8a01d04
shkimm5189 <shkimm5189@gmail.com>
2021-01-25 16:46:26 +0900

    b11654 아스키 코드  - 문자열

--- baekjoon/src/b11654/Main.java
+++ baekjoon/src/b11654/Main.java
@@ -1,31 +1,20 @@
-package b17298;
+package b11654;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-/*
- * 
- * 
- */
+
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int repeat = Integer.parseInt(br.readLine());
-		Stack<Integer> st = new Stack<>();
-		String[] str = br.readLine().split(" ");
-		
-		
-		
-		
-		bw.write("-1");
+		int a = br.readLine().charAt(0);
+		bw.write(a);
+		System.out.println(a);
 		bw.flush();
 		bw.close();
-	}
+		}
 }
