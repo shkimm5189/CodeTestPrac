93c2242ebd324cf89e8567dc01669edaf0c64191
shkimm5189 <shkimm5189@gmail.com>
2021-02-03 19:38:39 +0900

    B1546 평균

--- baekjoon/src/b1546_평균/Main.java
+++ baekjoon/src/b1546_평균/Main.java
@@ -1,5 +1,33 @@
 package b1546_평균;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int repeat = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		double[] arr = new double[repeat];
+		
+		for(int i = 0 ; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		double max = arr[arr.length-1];
+		double sum = 0 ;
+		for(int i = 0 ; i < arr.length; i++) {
+			arr[i] = (arr[i]/max)*100;
+			System.out.println(arr[i]);
+			sum+=arr[i];
+		}
+		System.out.println(sum/repeat);
+		
+	}
 }
--- baekjoon/src/b3052_나머지/Main.java
+++ baekjoon/src/b3052_나머지/Main.java
@@ -1,5 +1,25 @@
 package b3052_나머지;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[10];
+		int cnt=1;
+		for(int i = 0 ; i < 10; i++) {
+			int n = Integer.parseInt(br.readLine());
+			arr[i] = n%42;
+		}
+		Arrays.sort(arr);
+		for(int i = 0 ; i < arr.length-1 ; i++) {
+			if( arr[i] != arr[i+1]  ) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
 }
