4633e6ec83eab1ce898a4942fcc236df7e28e2cf
shkimm5189 <shkimm5189@gmail.com>
2021-02-16 14:30:43 +0900

    B1463 1로 만들기
    시간 초과 발생 dfs 방식을 구현 했으나 입력 값이 커질경우 시간 초과 발생

--- baekjoon/src/b1463_1로만들기/Main.java
+++ baekjoon/src/b1463_1로만들기/Main.java
@@ -4,21 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Main {
 	
 	
-	static int[] dp;
+	static int min;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int val = Integer.parseInt(br.readLine());
-		dp = new int[val+1];
+		min = val;
+		dfs(val,0);
 		
+		System.out.println("result "+min);
 	}
-	static int searching(int val , int depth) {
+	
+	static void dfs(int val, int depth) {
 		if(val == 1) {
-			return 0;
-		} 
-		
+			min = Math.min(min ,depth);
+			return ;
+		}
+		if(val%3 == 0) {
+			dfs(val/3,depth+1);
+		}
+		if(val%2 == 0) {
+			
+			dfs(val/2,depth+1);
+		}
+		dfs(val-1,depth+1);
 	}
 }
