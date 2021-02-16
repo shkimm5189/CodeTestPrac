e77a3a98484bd4052e84cfde70d737182b2a68b2
shkimm5189 <shkimm5189@gmail.com>
2021-02-16 13:34:48 +0900

    B14501 퇴사 해결
    dp에 대한 숙련도가 많이 부족한것같음...

--- baekjoon/src/b14501_퇴사/Main.java
+++ baekjoon/src/b14501_퇴사/Main.java
@@ -5,6 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
+class Commu {
+	int day;
+	int price;
+	
+	Commu(int day , int price){
+		this.day = day;
+		this.price = price;
+	}
+}
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,29 +22,21 @@ public class Main {
 		
 		
 		int repeat = Integer.parseInt(br.readLine());
-		int[] day = new int[repeat];
-		int[] price = new int[repeat];
+		Commu[] com = new Commu[repeat];
+		int[] dp = new int[repeat+1];
 		for(int i = 0 ; i < repeat; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			day[i] = Integer.parseInt(st.nextToken());
-			price[i] = Integer.parseInt(st.nextToken());
+			com[i] = new Commu(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
 		}
 		
-		int lastDay = 0 ;
-		int result = 0;
 		for(int i = 0 ; i < repeat; i++) {
-			lastDay = i;
-			int sum = 0;
-			for(int j =lastDay ; j < repeat; j++) {
-				if(lastDay+day[lastDay] < repeat+1) {
-					sum += price[lastDay];
-					lastDay += day[lastDay];
-				}
-				
+			if(i+ com[i].day <= repeat) {
+				dp[i+com[i].day] = Math.max(dp[i+com[i].day], dp[i]+com[i].price);
 			}
-			result = Math.max(result, sum);
+			dp[i+1] =Math.max(dp[i], dp[i+1]);
+			
 			
 		}
-		System.out.println(result);
+		System.out.println(dp[repeat]);
 	}
 }
