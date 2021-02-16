1e2fba9379e4f4ac633fa4fad775a17edd130596
shkimm5189 <shkimm5189@gmail.com>
2021-02-16 16:26:56 +0900

    B13458 시험 감독 해결
    문제의 반례를 잘 파악할 필요가 있다.

---
+++ baekjoon/src/b13458_시험감독/Main.java
@@ -0,0 +1,34 @@
+package b13458_시험감독;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] classRoom = new int[N];
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0 ; i < N; i++) {
+			classRoom[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine()," ");
+		int fir =Integer.parseInt(st.nextToken());
+		int sec =Integer.parseInt(st.nextToken());
+		System.out.println(fir+""+sec);
+long cnt = 0;
+		for(int i = 0 ; i < classRoom.length;i++) {
+			classRoom[i] -= fir;
+			cnt++;
+			cnt += classRoom[i]/sec;
+			if(classRoom[i]%sec != 0) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
