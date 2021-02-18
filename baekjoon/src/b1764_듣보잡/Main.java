bf31180ac9d685b5f89173b42fd958e08f737691
shkimm5189 <shkimm5189@gmail.com>
2021-02-18 14:53:40 +0900

    B1764 듣보잡 해결

---
+++ baekjoon/src/b1764_듣보잡/Main.java
@@ -0,0 +1,44 @@
+package b1764_듣보잡;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<String> noHear = new ArrayList<>();
+		ArrayList<String> result = new ArrayList<String>();
+		int cnt = 0 ;
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int H = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		
+		while( H-- > 0) {
+			noHear.add( br.readLine());
+
+		}
+		
+		Collections.sort(noHear);
+		while( S-- > 0) {
+			String find = br.readLine();
+			if(Collections.binarySearch(noHear, find) >= 0) {
+				
+				result.add(find);
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		Collections.sort(result);
+		for(String s : result) {
+			System.out.println(s);
+		}
+	}
+
+}
