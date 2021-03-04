46b907f83ad0e8010e487b42c8e8f0b578aa3f0b
shkimm5189 <shkimm5189@gmail.com>
2021-03-04 17:43:38 +0900

    B13305 주유소 해결
    그리디 문제 였음 다른건 문제가 안됬으나
    값이 커질때 변수를 적절히 지정해야함
    이 경우는 long이였음

---
+++ baekjoon/src/b13305_주유소/Main.java
@@ -0,0 +1,51 @@
+package b13305_주유소;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Node{
+	int price;
+	int length;
+	
+	
+}
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st ;
+		
+		int N = Integer.parseInt(br.readLine());
+		Node[] node = new Node[N]; 
+		int sum = 0 ;
+		
+		// 객체 배열 초기화.
+		for(int i = 0 ; i < N; i++) {
+			node[i] = new Node();
+		}
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0 ; i < N-1; i++) {
+			node[i].length = Integer.parseInt(st.nextToken());
+		
+		}
+		
+		st= new StringTokenizer(br.readLine()," ");
+		for(int i = 0 ; i < N; i++) {
+			node[i].price = Integer.parseInt(st.nextToken());
+		}
+		
+		sum = node[0].price*node[0].length;
+		int minPrice = node[0].price ;
+		for(int i = 1 ; i < node.length; i++) {
+			if(node[i].price < node[i-1].price ) {
+				minPrice = Math.min(minPrice , node[i].price);
+				
+			}
+			sum += (minPrice * node[i].length);
+		}
+		System.out.println(sum);
+		
+		
+	}
+}
