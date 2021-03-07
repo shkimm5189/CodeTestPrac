c1d41baa0f8292f43a2991faf92ec2596b784512
shkimm5189 <shkimm5189@gmail.com>
2021-03-07 21:09:20 +0900

    B1946 신입사원 해결
    순위가 중복이 안되므로 배열의 주소를 순위 x로 하여 y값을 넣어서 해겷할수있다

--- baekjoon/src/b1946_신입사원/Main.java
+++ baekjoon/src/b1946_신입사원/Main.java
@@ -3,104 +3,36 @@ package b1946_신입사원;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
-class Score implements Comparable<Score>{
-	int docu;
-	int inter;
-	int total;
-	Score(int docu,int inter){
-		this.docu = docu;
-		this.inter = inter;
-		this.total = docu+inter;
-	}
-
-
-	@Override
-	public int compareTo(Score s) {
-
-		return this.total - s.total;
-	}
-}
-public class Main {
+public class Main{
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
 		int testCaseN = Integer.parseInt(br.readLine());
-		while(testCaseN-- > 0) {
-			int sameCnt = 1 ;
-			ArrayList<Score> sameList = new ArrayList<Score>();
-			ArrayList<Score> subList = null ;
+		StringBuilder sb =new StringBuilder();
+		StringTokenizer st ;
+		while(testCaseN-- > 0 ) {
 			int N = Integer.parseInt(br.readLine());
-			Score[] score = new Score[N];
-			int max = N;
-			for(int i = 0 ; i < N; i++) {
+			int[] arrRank = new int[N+1];
+			for(int i = 0 ;i < N; i++) {
 				st = new StringTokenizer(br.readLine()," ");
-				score[i] = new Score(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+				int docRank = Integer.parseInt(st.nextToken());
+				int interRank = Integer.parseInt(st.nextToken());
+				arrRank[docRank] = interRank;
 			}
-			Arrays.sort(score);
-			// total이 같은 구간이 있는지 확인
-			for(int i = 1 ; i < score.length; i++) {
-				if(score[i].total ==  score[i-1].total) {
-					if(sameList.isEmpty()) {
-						sameList.add(score[i-1]);
-					}
-					if(score[i].total != score[i-1].total && (i+1) < score.length && 
-							score[i].total == score[i+1].total) {
-						sameList.add(score[i]);
-					}
-					sameList.add(score[i]);
+			
+			int max = 1 ;
+			int temp = arrRank[1];
+			for( int i = 2 ; i <= N; i++) {
+				if(arrRank[i] < temp) {
+					max++;
+					temp = arrRank[i];
 				}
 			}
-			if(!sameList.isEmpty()) {
-				// total이 같은 구간에서 첫 번째로 등장하는 구간만 필요함
-				for(int i = 1 ; i < sameList.size();i++) {
-					if( sameList.get(i).total !=sameList.get(i-1).total ) {
-						subList = new ArrayList<Score>(sameList.subList(0, i));
-						break;
-					}
-					subList = new ArrayList<Score>(sameList.subList(0, i+1));
-				}
-
-
-				// 잘라낸 리스트에서동률이 있느지  확인;
-				Collections.sort(subList,new Comparator<Score>() {
-					@Override
-					public int compare(Score o1, Score o2) {
-						// TODO Auto-generated method stub
-						return o1.docu - o2.docu;
-					}
-				});
-
-				for(int i = 0 ; i <  subList.size()/2; i++) {
-					if(subList.get(i).docu == subList.get((subList.size()-1)-i).inter) {
-						sameCnt+=2;
-					}
-				}
-			} else {
-				System.out.println(1);
-				break;
-			}
-
-			int result = 0;
-			for( int i = 1 ; i <score.length; i++) {
-				if(score[i].total == score[i-1].total && score[i].total < score[i+1].total) {
-					result = i+1;
-				}
-			}
-			// 마무리
-			if(sameCnt == 1) {
-				
-				
-				System.out.println(result - sameCnt);
-			} else {
-				System.out.println(result);
-			}
+			sb.append(max).append("\n");
+			
 		}
+		
+		System.out.println(sb);
 	}
 }
