47c70255dce3c379ea96f13f61ce53e944779c39
shkimm5189 <shkimm5189@gmail.com>
2021-03-08 18:13:28 +0900

    B4796 캠핑 해결
    그리디 문제

---
+++ baekjoon/src/b4796_캠핑/Main.java
@@ -0,0 +1,31 @@
+package b4796_캠핑;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int cnt = 0 ;
+		while(true) {
+			st = new StringTokenizer(br.readLine()," ");
+			cnt++;
+			long L = Long.parseLong(st.nextToken());
+			long P = Long.parseLong(st.nextToken());
+			long V = Long.parseLong(st.nextToken());
+			if(L == 0)  break;
+			
+			long possibleDay = V / P;
+			
+			possibleDay = possibleDay * L;
+			
+			possibleDay = possibleDay + (V%P);  
+			sb.append("Case "+ cnt+": "+possibleDay+"\n");
+		}
+		System.out.println(sb);
+	}
+}
